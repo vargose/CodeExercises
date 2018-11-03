f0ae04813bbca273ef9b041c0e32be01b2c51a03
Mitchell Harris <mitchell.harris2@gmail.com>
2018-11-03 13:15:30 -0500

    Adding base project with eslint and jest for code quality and testing - Mitch Harris

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "extends": ["airbnb-base","jest/recommended"],
+    "plugins": ["import","jest"],
+    "env": {
+        "node": true,
+        "jest/globals": true
+      }
+};
