3cc4cc335247a7748f0c21882fa43a378f033cca
Mitchell Harris <mitchell.harris2@gmail.com>
2018-11-03 13:44:26 -0500

    fixing lint file and adjusting for my style - Mitch Harris

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,12 @@
 module.exports = {
-    "extends": ["airbnb-base","jest/recommended"],
+    "extends": ["airbnb-base","plugin:jest/recommended"],
     "plugins": ["import","jest"],
     "env": {
         "node": true,
         "jest/globals": true
-      }
+      },
+    "rules": {
+        "linebreak-style":0,
+        "indent":[2,4]
+    }
 };
