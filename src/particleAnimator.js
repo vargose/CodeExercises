d0d39398e2f605194f2832d45b2cb9ba5da9e24e
Mitchell Harris <mitchell.harris2@gmail.com>
2018-11-04 00:29:12 -0500

    animating particles at speed 1 - Mitch Harris

---
+++ src/particleAnimator.js
@@ -0,0 +1,47 @@
+const particleAnimator = {};
+
+function render(particles, length) {
+    const frame = '.'.repeat(length).split('');
+    for (let i = 0; i < particles.length; i += 1) {
+        frame[Math.abs(particles[i]) - 1] = 'X';
+    }
+    return frame.join('');
+}
+
+function readInitialState(init) {
+    const particles = [];
+    for (let i = 0; i < init.length; i += 1) {
+        if (init[i] === 'L') {
+            particles.push(-(i + 1));
+        } else if (init[i] === 'R') {
+            particles.push(i + 1);
+        }
+    }
+    return particles;
+}
+
+function move(particles, length) {
+    const newPositions = [];
+    for (let i = 0; i < particles.length; i += 1) {
+        const newPosition = particles[i] + 1;
+        if (Math.abs(newPosition) > 0 && newPosition <= length) {
+            newPositions.push(newPosition);
+        }
+    }
+    return newPositions;
+}
+
+particleAnimator.animate = (speed, init) => {
+    let particles = readInitialState(init);
+    const frames = [render(particles, init.length)];
+    if (speed > 0) {
+        while (particles.length > 0) {
+            particles = move(particles, init.length);
+            frames.push(render(particles, init.length));
+        }
+    }
+
+    return frames;
+};
+
+module.exports = particleAnimator;
---
+++ src/particleAnimator.test.js
@@ -0,0 +1,43 @@
+const particleAnimator = require('./particleAnimator');
+
+describe('particleAnimator', () => {
+    it('returns empty array for empty init', () => {
+        expect(particleAnimator.animate(1, '')).toEqual(['']);
+    });
+
+    it('replaces L and R with X', () => {
+        expect(particleAnimator.animate(0, '.L.R.')).toEqual(['.X.X.']);
+    });
+
+    it('moves the particles Right', () => {
+        expect(particleAnimator.animate(1, 'R.')).toEqual(['X.', '.X', '..']);
+    });
+
+    it('moves the particles Left', () => {
+        expect(particleAnimator.animate(1, '.L')).toEqual(['.X', 'X.', '..']);
+    });
+
+    it('moves lots of particles', () => {
+        expect(particleAnimator.animate(1, 'LRRL.LR.LRR.R.LRRL.'))
+            .toEqual([
+                'XXXX.XX.XXX.X.XXXX.',
+                '..XXX..X..XX.X..XX.',
+                '.X.XX.X.X..XX.XX.XX',
+                'X.X.XX...X.XXXXX..X',
+                '.X..XXX...X..XX.X..',
+                'X..X..XX.X.XX.XX.X.',
+                '..X....XX..XX..XX.X',
+                '.X.....XXXX..X..XX.',
+                'X.....X..XX...X..XX',
+                '.....X..X.XX...X..X',
+                '....X..X...XX...X..',
+                '...X..X.....XX...X.',
+                '..X..X.......XX...X',
+                '.X..X.........XX...',
+                'X..X...........XX..',
+                '..X.............XX.',
+                '.X...............XX',
+                'X.................X',
+                '...................']);
+    });
+});
